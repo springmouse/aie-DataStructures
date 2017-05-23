791bc075541c3b2e097bd827dd144d2af5ce8552
springmouse <callum.dunstone@gmail.com>
2017-05-23 16:40:08 +1000

    Completed List??
    added lots of new features tired dont cear this is bad comment

--- Custom Containers/List.h
+++ Custom Containers/List.h
@@ -16,8 +16,8 @@ private:
             previous = next = nullptr;
         };
 
-        ListNode * previous;
         ListNode * next;
+        ListNode * previous;
 
         T obj;
     };
@@ -34,19 +34,17 @@ public:
 
     ~List();
 
-    void PushFront(T value);
+    void PushFront(const T & value);
 
-    void PushBack(T value);
-
-    void insert(ListNode & node, T value);
+    void PushBack(const T & value);
 
     void PopBack();
 
     void PopFront();
 
-    void Erase(ListNode & node);
+    void Remove(const T & value);
 
-    void Remove(T value);
+    void RemoveAll(const T & value);
 
     void DeleteAll();
 
@@ -54,9 +52,21 @@ public:
 
     T & Last();
 
+    T & GetElemeantAt(const int & P);
+
+    int GetLength();
+
+#pragma region PutOnDelay
+   /* 
+   ListNode List<T>::GetNodeByValue(T value);
+
+    void insert(ListNode & node, T value);
+    
     ListNode & Begin();
 
-    ListNode & LEnd();
+    ListNode & End();
+    */
+#pragma endregion
 
 };
 
@@ -114,7 +124,7 @@ void List<T>::DeleteAll()
 }
 
 template<class T>
-void List<T>::PushFront(T value)
+void List<T>::PushFront(const T & value)
 {
     if (m_listCount == 0)
     {
@@ -138,7 +148,7 @@ void List<T>::PushFront(T value)
 }
 
 template<class T>
-void List<T>::PushBack(T value)
+void List<T>::PushBack(const T & value)
 {
     if (m_listCount == 0)
     {
@@ -162,13 +172,262 @@ void List<T>::PushBack(T value)
 }
 
 template<class T>
-void List<T>::insert(ListNode & node, T value)
+void List<T>::PopBack()
 {
+    if (m_listCount > 0)
+    {
+        ListNode * holder = m_last->previous;
+
+        delete m_last;
+
+        holder->next = nullptr;
+
+        m_last = holder;
+
+        m_listCount--;
+    }
+}
+
+template<class T>
+void List<T>::PopFront()
+{
+    if (m_listCount > 0)
+    {
+        ListNode * holder = m_first->next;
+
+        delete m_first;
+
+        holder->previous = nullptr;
+
+        m_first = holder;
+
+        m_listCount--;
+    }
+}
+
+template<class T>
+void List<T>::Remove(const T & value)
+{
+    ListNode * node = m_first;
+    
+    if (m_listCount == 1 && node->obj == value)
+    {
+        DeleteAll();
+        return;
+    }
+
+    while (true && m_listCount > 1)
+    {
+        if (node->obj == value)
+        {
+            ListNode * holder;
+
+            if (node->next != nullptr)
+            {
+                holder = node->next;
+
+                holder->previous = node->previous;
+
+                delete node;
+
+                if (holder->previous != nullptr)
+                {
+                    ListNode * nodeHolder = holder->previous;
+                    nodeHolder->next = holder;
+                }
+                else
+                {
+                    m_first = holder;
+                }
+
+                m_listCount--;
+                return;
+
+            }
+            else if (node->previous != nullptr)
+            {
+                holder = node->previous;
+
+                holder->next = node->next;
+
+                delete node;
+
+                if (holder->next != nullptr)
+                {
+                    ListNode * nodeHolder = holder->next;
+                    nodeHolder->previous = holder;
+                }
+                else
+                {
+                    m_last = holder;
+                }
+
+                m_listCount--;
+                return;
+
+            }
+        }
+        else if (node->next == nullptr)
+        {
+            return;
+        }
+
+        node = node->next;
+    }
+        
+}
+
+template<class T>
+void List<T>::RemoveAll(const T & value)
+{
+    ListNode * node = m_first;
+
+    if (m_listCount == 1 && node->obj == value)
+    {
+        DeleteAll();
+        return;
+    }
+
+    while (true && m_listCount > 1)
+    {
+        if (node->obj == value && node != nullptr)
+        {
+            ListNode * holder;
+
+            if (node->next != nullptr)
+            {
+                holder = node->next;
+
+                holder->previous = node->previous;
+
+                delete node;
+
+                if (holder->previous != nullptr)
+                {
+                    ListNode * nodeHolder = holder->previous;
+                    nodeHolder->next = holder;
+                }
+                else
+                {
+                    m_first = holder;
+                }
+
+                m_listCount--;
+                node = holder;
+
+            }
+            else if (node->previous != nullptr)
+            {
+                holder = node->previous;
+
+                holder->next = node->next;
+
+                delete node;
+
+                if (holder->next != nullptr)
+                {
+                    ListNode * nodeHolder = holder->next;
+                    nodeHolder->previous = holder;
+                }
+                else
+                {
+                    m_last = holder;
+                }
+
+                m_listCount--;
+                node = holder;
+
+            }
+        }
+        else if (node->next == nullptr)
+        {
+            return;
+        }
+        else
+        {
+            node = node->next;
+        }
+
+    }
 
 }
 
 template<class T>
-void List<T>::PopBack()
+T & List<T>::First()
 {
-
+    return m_first->obj;
 }
+
+template<class T>
+T & List<T>::Last()
+{
+    return m_last->obj;
+}
+
+template<class T>
+T & List<T>::GetElemeantAt(const int & P)
+{
+    if (P <= m_listCount)
+    {
+        ListNode * holder = m_first;
+        for (int i = 0; i <= P; i++)
+        {
+            if (i == P)
+            {
+                return holder->obj;
+            }
+            else
+            {
+                holder = holder->next;
+            }
+        }
+    }
+    else
+    {
+        throw;
+    }
+}
+
+template<class T>
+int List<T>::GetLength()
+{
+    return m_listCount;
+}
+
+#pragma region PutOnDelay
+//template<class T>
+//ListNode List<T>::GetNodeByValue(T value)
+//{
+//    ListNode * node = m_first->next;
+//
+//    if (node == nullptr)
+//    {
+//        return nullptr;
+//    }
+//
+//    if (node->obj == value)
+//    {
+//        return node;
+//    }
+//    else
+//    {
+//        node = node->next
+//    }
+//
+//
+//}
+//
+//template<class T>
+//void List<T>::insert(ListNode & node, T value)
+//{
+//    ListNode * NN = new ListNode();
+//
+//    NN->previous = node->previous->previous;
+//
+//    NN->next = node;
+//
+//    NN->obj = value;
+//
+//    node->previous = NN;
+//}
+#pragma endregion
--- Data Structure Tester/Main.cpp
+++ Data Structure Tester/Main.cpp
@@ -7,16 +7,43 @@ void main()
 
     List<int> test = List<int>();
 
-    
-    while (true)
+    test.PushBack(11);
+    test.PushBack(9);
+    test.PushBack(5);
+    test.PushBack(4);
+    test.PushBack(6);
+
+    test.PopBack();
+    test.PopFront();
+
+    test.Remove(4);
+    test.Remove(9);
+
+    test.PushFront(100);
+    test.PushFront(111);
+    test.PushFront(112);
+    test.PushFront(123);
+
+    test.Remove(112);
+
+    test.PushBack(10);
+    test.PushBack(10);
+
+    test.PushFront(10);
+    test.PushFront(10);
+
+    test.RemoveAll(10);
+
+    int & testlingz = test.First();
+
+    testlingz = 777;
+
+    int & blargh = test.GetElemeantAt(2);
+
+    blargh = 66;
+
+    for (int i = 0; i < test.GetLength(); i++)
     {
-        for (int i = 0; i < 1000000; i++)
-        {
-            test.PushBack(rand());
-        }
-
-        test.DeleteAll();
+        test.GetElemeantAt(i) = i;
     }
-
-
 }
